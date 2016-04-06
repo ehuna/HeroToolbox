07b3d2167546a6215270337d048517e8067babe3
ehuna <hero@stealthlab.net>
2016-04-06 13:50:58 -0700

    Add a Unit Tests project and some tests on Has()

---
+++ Hero.Toolbox.Tests.Unit/Common/Pcl/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StringExtensionsTests.cs" company="hero">
+//   MIT License
+// </copyright>
+// <summary>
+//   Defines the StringExtensionsTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Hero.Toolbox.Tests.Unit.Common.Pcl.Extensions
+{
+    using Hero.Toolbox.Common.Pcl.Extensions;
+    using Xunit;
+
+    /// <summary>
+    /// The string extensions tests.
+    /// </summary>
+    public class StringExtensionsTests
+    {
+        /// <summary>
+        /// The when a string is null or empty has returns false tests.
+        /// </summary>
+        /// <param name="inputString">
+        /// The input string.
+        /// </param>
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+        public void WhenAStringIsNullOrEmptyHasReturnsFalse(string inputString)
+        {
+            var expectedResult = false;
+            var result = inputString.Has();
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
