551264ac39ccecaf48c9d4a2d0f746335b33c67a
ehuna <hero@stealthlab.net>
2016-04-06 13:43:02 -0700

    Add a Has() string extension

---
+++ src/Hero.Toolbox.Common.Pcl/Extensions/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StringExtensions.cs" company="hero">
+//   MIT License
+// </copyright>
+// <summary>
+//   Defines the StringExtensions type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Hero.Toolbox.Common.Pcl.Extensions
+{
+    /// <summary>
+    /// The string extensions.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns true if string is NOT (null, empty or white space)
+        /// </summary>
+        /// <param name="source">
+        /// The source.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public static bool Has(this string source)
+        {
+            return !string.IsNullOrWhiteSpace(source);
+        }
+    }
+}
